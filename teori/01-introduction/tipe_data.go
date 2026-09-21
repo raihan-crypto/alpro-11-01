d50e7a56b1cea9a01c7cf10036cf435b7e93c1c6
RaihanAR <raihanahmadrahmadhani@gmail.com>
2026-09-21 13:18:23 +0700

    hello word & tipe data

---
+++ teori/01-introduction/tipe_data.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var umur int8
+	var anakKe int8
+	var saldo int64
+
+
+	umur = 18
+	anakKe = 1
+	saldo = 100000000
+
+	fmt.Println("Umur saya adalah", umur)
+	fmt.Println("Saya adalah anak ke- ", anakKe)
+	fmt.Println("Saldo saya adalah", saldo)
+}
